04e809399a4bc6d7de2613d67efa5ae638491506
Mr. Gart <143269684+kutukomputer32@users.noreply.github.com>
2023-11-03 13:33:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def verify_user(chat_id):
 	return True
 
 def extract_arg(arg):
-    return arg.split(' ')[1]
+    return arg.split(' ', 1)[1]
 
 def lokasi():
 	print('starting termux-location...')
@@ -125,7 +125,7 @@ def vibrate(message):
 
 # Menghandle Pesan /volume
 @bot.message_handler(commands=['volume'])
-def vibrate(message):
+def volume(message):
 	# membalas pesan
 	if verify_user(message.chat.id):
 		print('failed: chat id not valid')
@@ -133,9 +133,72 @@ def vibrate(message):
 
 	arg = extract_arg(message.text)
 
-	os.system(f'termux-vibrate {arg}')
+	os.system(f'termux-volume music {arg}')
 
 	bot.reply_to(message, f'Berhasil mengubah volume menjadi {arg}')
+
+# Menghandle Pesan /userlist
+@bot.message_handler(commands=['userlist'])
+def userlist(message):
+	# membalas pesan
+	if verify_user(message.chat.id):
+		print('failed: chat id not valid')
+		return
+
+	print(users)
+
+	bot.reply_to(message, f'User List\n{users}')
+
+# Menghandle Pesan /adduser
+@bot.message_handler(commands=['adduser'])
+def adduser(message):
+	# membalas pesan
+	if verify_user(message.chat.id):
+		print('failed: chat id not valid')
+		return
+
+	arg = extract_arg(message.text)
+
+	users.append(int(arg))
+
+	print(users)
+
+	bot.reply_to(message, f'Berhasil menambahkan pengguna {int(arg)}')
+
+# Menghandle Pesan /userdel
+@bot.message_handler(commands=['userdel'])
+def userdel(message):
+	# membalas pesan
+	if verify_user(message.chat.id):
+		print('failed: chat id not valid')
+		return
+
+	arg = extract_arg(message.text)
+
+	#del users[int(arg)]
+
+	users.remove(int(arg))
+
+	print(users)
+
+	bot.reply_to(message, f'Berhasil menghapus pengguna {int(arg)}')
+
+# Menghandle Pesan /message
+@bot.message_handler(commands=['message'])
+def send_msg(message):
+	# membalas pesan
+	if verify_user(message.chat.id):
+		print('failed: chat id not valid')
+		return
+
+	arg = extract_arg(message.text)
+
+	bot.reply_to(message, f'Mengirim pesan "{arg}"')
+	bot.reply_to(message, 'Menunggu jawaban...')
+	result = subprocess.run([f'./termux-location {arg}'], stdout=subprocess.PIPE).stdout
+	print('get JSON')
+	result_json = json.loads(result)
+	bot.reply_to(message, result_json['text'])
 
 # Menghandle Pesan /info
 @bot.message_handler(commands=['info'])
@@ -179,6 +242,11 @@ def bot_exit(message):
 
 	bot.reply_to(message, 'exit...')
 	os.system('killall python3')
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+	result = subprocess.run([message.text], stdout=subprocess.PIPE).stdout
+	bot.reply_to(message, result)
 
 while True:
 	try:
